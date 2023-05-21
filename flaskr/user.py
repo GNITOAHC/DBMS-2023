aef3d0a2a42c699a8005557adff4ad0fb5dea160
kylai02 <dennislai120@gmail.com>
2023-05-21 15:51:29 +0800

    refactor: user return and list

--- flaskr/user.py
+++ flaskr/user.py
@@ -101,7 +101,6 @@ def user_rent():
         return redirect(
             url_for("api.user_rent_api", bike_serial=bike_serial, card_id=card_id)
         )
-
     # get: generate the form to gather the renting info
     else:
         # query data from db
@@ -117,8 +116,75 @@ def user_rent():
 
         return render_template("user_rent.html", user=user)
 
-# # get and post the returning info
-# @app.route("/user/return", methods=["POST", "GET"])
-# def user_return():
-#     # TODO:
-#     return "Return "
+# FROM HERE
+# get and post the returning info
+@user_blueprint.route("/return", methods=["POST", "GET"])
+def user_return():
+    # get cardId from request arguments
+    card_id = request.args.get("card_id")
+    if card_id is None:
+        abort(400, "Parameter not found")
+
+    # post: redirect to renting api
+    if request.method == "POST":
+        is_broken = request.form["ibf"]
+        park_loc = request.form["plf"]
+        return redirect(
+            url_for(
+                "api.user_return_api",
+                card_id=card_id,
+                is_broken=is_broken,
+                park_loc=park_loc,
+            )
+        )
+    # get: generate the form to gather the renting info
+    else:
+        # query data from db
+        cursor = db.connection.cursor()
+        cursor.execute(f'''
+            SELECT * FROM User
+            WHERE CardID = {card_id};
+        ''')
+        user = cursor.fetchone()
+        if user is None:
+            abort(404, "User not found")
+        
+        cursor.execute(f'''
+            SELECT Name FROM Location;
+        ''')
+        stops = cursor.fetchall()
+        cursor.close()
+
+        return render_template("user_return.html", user=user, stops=stops)
+
+# renting api
+@user_blueprint.route("/list", methods=["POST", "GET"])
+def user_list():
+    # get cardId from request arguments
+    card_id = request.args.get("card_id")
+    if card_id is None:
+        abort(400, "Parameter not found")
+
+    # post: redirect to renting api
+    if request.method == "POST":
+        input_loc = request.form["lf"]
+        return redirect(url_for("api.user_list_api", card_id=card_id, input_loc=input_loc))
+    # get: generate the form to gather the renting info
+    else:
+        # query data from db
+        cursor = db.connection.cursor()
+        cursor.execute(f'''
+            SELECT * FROM User
+            WHERE CardID = {card_id};
+        ''')
+        user = cursor.fetchone()
+        if user is None:
+            abort(404, "User not found")
+        
+        cursor.execute(f'''
+            SELECT Name FROM Location;
+        ''')
+        stops = cursor.fetchall()
+        cursor.close()
+
+        return render_template("user_list_form.html", user=user, stops=stops)
