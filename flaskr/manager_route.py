7d7f818202a0259471c04ae0dc23a7bf46c21852
neo-lu-15 <neo901115@gmail.com>
2023-06-14 16:27:46 +0800

    make manger work and revise html for manager

--- flaskr/manager_route.py
+++ flaskr/manager_route.py
@@ -1,27 +1,31 @@
 from flask import Blueprint, render_template, jsonify, request, abort, url_for, redirect
 from database import db
 
-
 manager_blueprint = Blueprint('manager', __name__, template_folder='templates')
 
 
 
 @manager_blueprint.route('/manager/check_subordinates', methods=['GET'])
 def check_subordinates():
-    result = db.session.execute("SELECT * FROM Employee;")
-    employees = result.fetchall()
-    return render_template('subordinates.html', employees=employees)
+    cursor = db.connection.cursor()
+    cursor.execute("SELECT * FROM Employee;")
+    result = cursor.fetchall()
+    print(result)
+    return render_template('subordinates.html', employees=result)
 
-@manager_blueprint.route('/manager/click_subordinate/<int:ssn>')
-def click_subordinate(ssn):
-    result = db.session.execute("""
+@manager_blueprint.route('/manager/click_subordinate', methods=['GET'])
+def click_subordinate():
+    ssn = request.args.get("ssn")
+    cursor = db.connection.cursor()
+    cursor.execute(f"""
         SELECT Bike.Serial_num, Location.Name
         FROM Employee
         JOIN Bike ON Bike.Maintenance_Employee = Employee.Ssn
         JOIN Location ON Location.Control_Employee = Employee.Ssn
-        WHERE Employee.Ssn = :emp_id;
-        """, {"emp_id": ssn})
-    employees = result.fetchall()
-    return render_template('click_subordinate.html', employees=employees)
+        WHERE Employee.Ssn = {ssn};
+        """)
+    result = cursor.fetchall()
+    print(result)
+    return render_template('click_subordinate.html', employees=result)
 
 
